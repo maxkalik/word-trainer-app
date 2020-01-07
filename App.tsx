827ac15810b5cd32839a32152bfc6770d5ae04ab
Max Kalik <maxkalik@gmail.com>
2020-01-07 17:47:09 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+
+declare var global: { HermesInternal: null | {} };
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Word Traner</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    height: '100%'
+  }
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,18 @@
+# Word Trainer
+
+I was inspired by [Lingualeo](https://github.com/LinguaLeo). But I need to have universal model where I can use an application with any laguages.
+
+## How to run
+
+To run iOS
+
+```bash
+npx react-native run-ios
+```
+
+To run Android
+
+```bash
+npx react-native run-android
+```
+
